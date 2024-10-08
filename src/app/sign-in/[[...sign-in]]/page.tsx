80d41fc99a4f5e09b61f2264726ed8b262ce6fe9
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-10-08 13:05:27 -0500

    sign in sign up pages clerk

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+﻿import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Smart Notes - Sign In",
+};
+
+export default function SignInPage() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <SignIn appearance={{ variables: { colorPrimary: "#0F172A" } }} />
+        </div>
+    );
+}
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+﻿import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Smart Notes - Sign Up",
+};
+
+export default function SignUpPage() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <SignUp appearance={{ variables: { colorPrimary: "#0F172A" } }} />
+        </div>
+    );
+}
