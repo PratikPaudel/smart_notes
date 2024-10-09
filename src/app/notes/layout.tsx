8299c635c2e6eedd35218cf3702dc056ca63ca30
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-10-08 23:04:44 -0500

    note and app layout files

---
+++ src/app/notes/layout.tsx
@@ -0,0 +1,14 @@
+﻿import NavBar from "@/app/notes/NavBar";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <NavBar />
+      <main className="m-auto max-w-7xl p-4">{children}</main>
+    </div>
+  );
+}
